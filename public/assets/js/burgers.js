7b994fe169e6fad8f3be85d7dcb507641aa56c5b
Tyler Roth <tylerroth1535@gmail.com>
2020-07-01 20:25:02 -0400

    updated index.handlebars to call correct burger_data. Also changed the burger.js file

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,60 +1,76 @@
-  
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-sleep").on("click", function(event) {
-    var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
-
-    var newSleepState = {
-      sleepy: newSleep
-    };
-
-    // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
-      type: "PUT",
-      data: newSleepState
-    }).then(
-      function() {
-        console.log("changed sleep to", newSleep);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
+$(document).ready(function() {
+    
+  $(".devour-form").on("submit", function(event) {
     event.preventDefault();
 
-    var newBurger = {
-      name: $("#ca").val().trim(),
-    };
+    var burger_id = $(this).children(".burger_id").val();
+    console.log(burger_id);
+    $.ajax({
+      method: "PUT",
+      url: "/burgers/" + burger_id
+    }).then(function(data) {
+      // reload page to display devoured burger in proper column
+      location.reload();
+    });
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
   });
+});  
+// // Make sure we wait to attach our handlers until the DOM is fully loaded.
+// $(function() {
+//   $(".change-sleep").on("click", function(event) {
+//     var id = $(this).data("id");
+//     var newSleep = $(this).data("newsleep");
 
-  $(".delete-burger").on("click", function(event) {
-    var id = $(this).data("id");
+//     var newSleepState = {
+//       sleepy: newSleep
+//     };
+
+//     // Send the PUT request.
+//     $.ajax("/api/cats/" + id, {
+//       type: "PUT",
+//       data: newSleepState
+//     }).then(
+//       function() {
+//         console.log("changed sleep to", newSleep);
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
+
+//   $(".create-form").on("submit", function(event) {
+//     // Make sure to preventDefault on a submit event.
+//     event.preventDefault();
+
+//     var newBurger = {
+//       name: $("#ca").val().trim(),
+//     };
+
+//     // Send the POST request.
+//     $.ajax("/api/burgers", {
+//       type: "POST",
+//       data: newBurger
+//     }).then(
+//       function() {
+//         console.log("created new burger");
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
+
+//   $(".delete-burger").on("click", function(event) {
+//     var id = $(this).data("id");
     
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted burger" + id)
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-});
+//     // Send the PUT request.
+//     $.ajax("/api/burgers/" + id, {
+//       type: "DELETE"
+//     }).then(
+//       function() {
+//         console.log("deleted burger" + id)
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
+// });
