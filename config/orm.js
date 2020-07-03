ab50303cf76b1bbdbb3fbfa95367de9b301337e5
Tyler Roth <tylerroth1535@gmail.com>
2020-07-02 22:34:35 -0400

    heroku deployment

--- config/orm.js
+++ config/orm.js
@@ -15,7 +15,7 @@ function printQuestionMarks(num) {
 }
 
 function objToSql(ob) {
-  // column1=value, column2=value2,...
+
   var arr = [];
 
   for (var key in ob) {
@@ -35,8 +35,7 @@ var orm = {
       cb(result);
     });
   },
-  // vals is an array of values that we want to save to cols
-  // cols are the columns we want to insert the values into
+
   create: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
@@ -56,8 +55,7 @@ var orm = {
       cb(result);
     });
   },
-  // objColVals would be the columns and values that you want to update
-  // an example of objColVals would be {name: panther, sleepy: true}
+
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
